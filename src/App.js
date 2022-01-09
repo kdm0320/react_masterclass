86f073de28cd99136ea41df2cc65b848236e75a2
Dongmin <kdm3578@gmail.com>
2022-01-09 20:24:08 +0900

    Styled-Components(as,attrs)

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="blue" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
