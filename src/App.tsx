3cbe27add9ea780ab229ab41dfec20c71c31027f
Dongmin <kdm3578@gmail.com>
2022-01-11 15:23:29 +0900

    coin_tracker_setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DymmyProps {
-  text: string;
-  other?: boolean;
-}
-
-function Dummy({ text, other = false }: DymmyProps) {
-  return <H1>{text}</H1>;
-}
-
-const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
+import Router from "./Router";
 
 function App() {
-  return (
-    <Container>
-      <Dummy other text="hello" />
-      <form>
-        <button onClick={onClick}>Click me</button>
-      </form>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
