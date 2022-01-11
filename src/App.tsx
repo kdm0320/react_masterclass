300f72f5a155675fa08b4f032e6d9409f19708dd
Dongmin <kdm3578@gmail.com>
2022-01-11 11:16:00 +0900

    TypeScript_Optional Props

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Circle borderColor="red" bgColor="teal" />
+      <Circle text="fuck" bgColor="tomato" />
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -11,14 +12,21 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
