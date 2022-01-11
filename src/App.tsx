d6a8539851a230ce163b6259c9456340ccaba282
Dongmin <kdm3578@gmail.com>
2022-01-11 14:37:29 +0900

    Recap

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,24 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DymmyProps {
+  text: string;
+  other?: boolean;
+}
+
+function Dummy({ text, other = false }: DymmyProps) {
+  return <H1>{text}</H1>;
+}
+
+const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
+
 function App() {
   return (
     <Container>
-      <H1>protected</H1>
+      <Dummy other text="hello" />
+      <form>
+        <button onClick={onClick}>Click me</button>
+      </form>
     </Container>
   );
 }
