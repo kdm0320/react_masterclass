19d74c35fd57e72bb5aaa5f2c12a01e5a89104d0
Dongmin <kdm3578@gmail.com>
2022-01-11 11:02:17 +0900

    typeScript with react_interface

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Circle from "./Circle";
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
