7a2a83c0a7946d2424fd8606814e56c46bf04a79
Dongmin <kdm3578@gmail.com>
2022-01-13 00:21:05 +0900

    To_Do_List  with react-hook-form Pt.2

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  Email: string;
+  FirstName: string;
+  LastName?: string;
+  Username: string;
+  Password: string;
+  Password1: string;
+}
 
 function ToDoList() {
-  const [toDo, setTodo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      Email: "@naver.com",
+    },
+  });
+  const onValid = (data: any) => {};
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input value={toDo} onChange={onChange} placeholder="Write a to do" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("Email", {
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.Email?.message}</span>
+        <input {...register("FirstName")} placeholder="First Name" />
+        <input {...register("LastName")} placeholder="Last Name" />
+        <input
+          {...register("Username", {
+            required: true,
+            minLength: { value: 5, message: "Your password is too short" },
+          })}
+          placeholder="Username"
+        />
+        <input {...register("Password")} placeholder="Password" />
+        <input {...register("Password1")} placeholder="Password1" />
         <button>Add</button>
       </form>
     </div>
