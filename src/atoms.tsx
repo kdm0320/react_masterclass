80f24f01970cbe020b6df6b1117bc2122c6bb10a
Dongmin <kdm3578@gmail.com>
2022-01-15 10:50:48 +0900

    Converter_how to use get,set in Selector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,31 +1,18 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  category: Categories;
-  id: number;
-}
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
+export const minutesState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const categoryState = atom<Categories>({
-  key: "caetgory",
-  default: Categories.TO_DO,
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector<number>({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minutesState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minutesState, minutes);
   },
 });
