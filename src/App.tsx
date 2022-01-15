440f297f7cbbcce6e896fdfe3c48bf74d19f5b7b
Dongmin <kdm3578@gmail.com>
2022-01-16 00:29:42 +0900

    Setup_Neflix_Clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,25 @@
-import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
-import { scale } from "style-value-types";
-
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  height: 150px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
-  gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
-`;
-
-const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-  const [id, setId] = useState<null | string>(null);
   return (
-    <Wrapper>
-      <Grid>
-        {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0, 0, 0,  0)" }}
-            animate={{ backgroundColor: "rgba(0, 0, 0,  0.5)" }}
-            exit={{ backgroundColor: "rgba(0, 0, 0,  0)" }}
-          >
-            <Box layoutId={id} style={{ width: 400, height: 200 }}></Box>
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-    </Wrapper>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
